44009cc03d89ecfbc9d16944ce217ea65e9a0291
ArianMoossavi <arianmoossavi@live.co.uk>
2022-08-23 23:37:01 +0100

    tickets functionality implemented

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main(){
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets uint = 1
+	var remainingTickets uint = 50
 
 	//Printf allows us to format a string with %v and \n creates a new line explicitly
 
@@ -13,12 +13,37 @@ func main(){
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName,userTickets)
+	
 
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	var bookings [50]string
+
+	//ask for input
+	fmt.Println("Enter your first name: ")
+	//& is a pointer and points to the memory address of where userName is stored
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	bookings[0] = firstName + " " + lastName 
+	
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",firstName,lastName,userTickets,email)
+	fmt.Printf("There are %v tickets remaining for %v\n",remainingTickets,conferenceName)
+	fmt.Println(bookings)
 }
